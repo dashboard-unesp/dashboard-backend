45af246c44ed969ccf2cee67255e4da727e25e19
DanilloRezende <danillo.rezende3@gmail.com>
2024-05-17 22:36:11 -0300

    feat: Leitura e registro de dados a partir de .dat

--- climate_data/data/views.py
+++ climate_data/data/views.py
@@ -1,24 +1,31 @@
 import fnmatch
 import os
+import shutil
 from datetime import datetime
 
-from data.models import *
 from django.conf import settings
+from django.db import transaction
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
+from ..data.models import *
+
 
 @csrf_exempt
 @require_http_methods(['GET'])
-def sync_production_documents(self):
+def sync_files(self):
 
     files_to_read = os.path.abspath(os.path.join(settings.BASE_DIR, '..' + "/PI_files/"))
 
     backup = files_to_read + "\BACKUP"
 
+    error = files_to_read + "\ERROR"
+
     climate_data_list = []
+
+    error_files = []
 
     files = fnmatch.filter(os.listdir(files_to_read), "*.dat")
 
@@ -29,7 +36,8 @@ def sync_production_documents(self):
             for line in lines:
                 value = line.split(",")
                 data_hora = value[0]
-                formatted_date = datetime.strptime(data_hora, '%Y-%m-%d  %H:%M:%S')
+                data_hora_sem_aspas = data_hora.replace('"', '')
+                formatted_date = datetime.strptime(data_hora_sem_aspas, '%Y-%m-%d %H:%M:%S')
                 register_id = float(value[1])
                 VelVent_ms = float(value[2])
                 DirVent = float(value[3])
@@ -53,8 +61,26 @@ def sync_production_documents(self):
                     Chuva=Chuva_mm_Tot,
                 )
                 climate_data_list.append(climate_data)
+
+            check_id = ClimateData.objects.get(RECORD=register_id)
+
+            if check_id:
+
+                error_files.append(file)              
+
+            else:
+
+                with transaction.atomic():      
+                    ClimateData.objects.bulk_create(climate_data_list)
+
+                shutil.move(os.path.join(files_to_read, file),
+                    os.path.join(backup, file))
                 
-            
-            ClimateData.objects.bulk_create(climate_data_list)
+                climate_data_list.clear()
+
+    for files_to_move in error_files:
+
+        shutil.move(os.path.join(files_to_read, files_to_move),
+            os.path.join(error, files_to_move))  
 
     return JsonResponse({"message": "Production Orders Created"},safe=False)
