cfafa0af8e2d085e16783d951f4bcf52afaa9661
Rafael <rafael_dvid@hotmail.com>
2024-07-12 22:27:44 -0300

    feat: implementing Modal for dashboard Table

--- unesp-dashboard/src/components/DashboardTable.tsx
+++ unesp-dashboard/src/components/DashboardTable.tsx
@@ -7,6 +7,17 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import { 
+    Button, 
+    Modal, 
+    ModalBody, 
+    ModalCloseButton, 
+    ModalContent, 
+    ModalFooter, 
+    ModalHeader, 
+    ModalOverlay, 
+    useDisclosure 
+} from "@chakra-ui/react"
 
 const items = [
     1,2,3,4,5,6,7,8,9,10
@@ -14,33 +25,50 @@ const items = [
 
 export function DashboardTable() {
 
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return(
-        <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[100px]">Invoice</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Method</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {
-                items.map((row) => {
-                        return (
-                            <TableRow key={row}>
-                                <TableCell className="font-medium">INV001</TableCell>
-                                <TableCell>Paid</TableCell>
-                                <TableCell>Credit Card</TableCell>
-                                <TableCell className="text-right">$250.00</TableCell>
-                            </TableRow>
-                        );  
-                    })
-                }
-            </TableBody>
-        </Table>
+        <>
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Invoice</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                    items.map((row) => {
+                            return (
+                                <TableRow key={row} onClick={onOpen}>
+                                    <TableCell className="font-medium">INV001</TableCell>
+                                    <TableCell>Paid</TableCell>
+                                    <TableCell>Credit Card</TableCell>
+                                    <TableCell className="text-right">$250.00</TableCell>
+                                </TableRow>
+                            );  
+                        })
+                    }
+                </TableBody>
+            </Table>
+
+            <Modal onClose={onClose} isOpen={isOpen} isCentered>
+                <ModalOverlay />
+                <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    something
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick={onClose}>Close</Button>
+                </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     );
 
 }
