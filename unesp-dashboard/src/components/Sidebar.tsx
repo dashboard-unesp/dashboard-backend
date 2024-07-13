0e1dca3eec301a871d18181883437cbd3da3d669
Rafael <rafael_dvid@hotmail.com>
2024-07-12 22:27:10 -0300

    feat: enhancing sidebar logic with icons and resizing

--- unesp-dashboard/src/components/Sidebar.tsx
+++ unesp-dashboard/src/components/Sidebar.tsx
@@ -1,38 +1,72 @@
 import { Button } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import { MdBuild, MdAccountCircle, MdHelp   } from "react-icons/md";
+
+type SidebuttonProps = {
+    title: string;
+    icon: JSX.Element;
+    label: string;
+}
+
+const subMenu: SidebuttonProps[] = [
+    {
+        title:'account',
+        icon: <MdAccountCircle size={18}/>,
+        label: 'Conta'
+    },
+    {
+        title:'config',
+        icon: <MdBuild size={18}/>,
+        label: 'Configurações'
+    },
+    {
+        title:'help',
+        icon: <MdHelp size={18}/>,
+        label: 'Ajuda'
+    },
+
+];
 
 function Sidebar(){
     const [isCollapsed, setIsCollapsed] = useState(false);
 
     useEffect(() => {
-        const sidebar = document.getElementById('sidebarContent');
+        const sidebarContent = document.getElementById('sidebarContent');
 
         if(!isCollapsed){
-            sidebar?.classList.add('w-[300px]');
+            sidebarContent?.classList.add('w-[300px]');
         }else{
-            sidebar?.classList.remove('w-[300px]');
+            sidebarContent?.classList.remove('w-[300px]');
         }
 
     }, [isCollapsed])
 
     return(
-        <nav className='max-w-fit'>
-            <div id='sidebarContent' className='flex flex-col justify-between h-full'>
+        <nav id='sidebarContainer' className='max-w-fit'>
+            <div id='sidebarContent' className='flex flex-col justify-between h-full ease-in-out duration-300 stretch-button'>
 
                 <ul className='flex flex-col p-4 gap-4'>
                     <Button onClick={() => setIsCollapsed(!isCollapsed)}/>
-                    <Button />
-                    <Button />
-                    <Button />
-                    <Button />
-                    <Button />
-                    <Button />
+                    <Button></Button>
+                    <Button></Button>
+                    <Button></Button>
+                    <Button></Button>
+                    <Button></Button>
+                    <Button></Button>
                 </ul>
 
                 <ul className='flex flex-col p-4 gap-4'>
-                    <Button />
-                    <Button />
-                    <Button />
+                    {
+                        subMenu.map((button) => (
+                            <Button>
+                                {
+                                    isCollapsed 
+                                        ? button.icon 
+                                        : <>{button.icon}<span className='pl-1'>{button.label}</span></>
+                                }
+                            </Button>
+                        ))
+                    }
                 </ul>
             </div>
 
