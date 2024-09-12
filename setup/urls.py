b7fbf08e31ab703acffbaca1eb2f69caae74cb16
Rafael <rafael_dvid@hotmail.com>
2024-09-12 17:34:04 -0300

    feat: creating me and User viewsets + routes

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+
 from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
 
-from stations.views import ClimateDataViewSet
+from users.views import UserViewSet, MeViewSet
+
 from stations.sync_data import sync_files
+from stations.views import ClimateDataViewSet
 
-from users.views import UserViewSet
+
+schema_view = get_swagger_view(title='Unesp Dashboard API')
 
 stations_router = DefaultRouter()
 users_router = DefaultRouter()
@@ -12,9 +17,11 @@ users_router = DefaultRouter()
 stations_router.register('climate', ClimateDataViewSet, basename="CDT")
 
 users_router.register('users', UserViewSet, 'UVT')
+users_router.register('me', MeViewSet, 'MVT')
 
 urlpatterns = [
     *stations_router.urls,
     *users_router.urls,
-    path('sync-files/', sync_files)
+    path('sync-files', sync_files),
+    path('docs', schema_view)
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
-from rest_framework.viewsets import ModelViewSet
+from django.http import HttpResponse
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
 from users.serializers import UserSerializer
 
-from rest_framework import status
-from rest_framework.response import Response
-
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    def list(self, request, *args, **kwargs):
+        return HttpResponse(status=status.HTTP_401_UNAUTHORIZED)
 
     def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
@@ -16,3 +22,11 @@ class UserViewSet(ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MeViewSet(ViewSet):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        return Response({'test': 'test'})
