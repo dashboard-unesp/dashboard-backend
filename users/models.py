2c4385a2a82847b73e4da293de5213e8c9f7e912
Rafael <rafael_dvid@hotmail.com>
2024-09-12 21:07:24 -0300

    chore: using abstract django user as base model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
+import bcrypt
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-class User(models.Model):
+class User(AbstractBaseUser):
 
     GENDER_CHOICES = (
         ('male', 'male'),
@@ -12,13 +14,10 @@ class User(models.Model):
     name = models.CharField(max_length=250, null=True, blank=True)
     password = models.CharField(max_length=128, null=False, blank=False)
     gender = models.CharField(max_length=100, choices=GENDER_CHOICES, null=False, blank=False, default='other')
-    birthdate = models.DateTimeField()
+    birthdate = models.DateTimeField(null=True, blank=True)
 
     created_by = models.DateTimeField(null=False, blank=False, auto_now=True, editable=False)
     updated_by = models.DateTimeField(null=False, blank=False, auto_now=True)
 
     def __str__(self) -> str:
-        return f'User: {self.name}'
-
-    class Meta:
-        pass
+        return f'User: {self.pk}'
