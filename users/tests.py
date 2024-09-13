8b493bf8105c6215f1d6dbefca90569f103ed1c5
Rafael <rafael_dvid@hotmail.com>
2024-09-12 21:08:29 -0300

    test: developing test file structure to further test cases

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    databases={'default'}
+
+    def setUp(self):
+        self.create_token()
+        self.create_test_data()
+
+
+    def create_token(self):
+        pass
+
+    def create_test_data(self):
+        pass
