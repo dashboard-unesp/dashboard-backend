52290c61c00c7cba5888eed6bc255830133fee2d
Rafael <rafael_dvid@hotmail.com>
2024-09-12 21:08:15 -0300

    feat: login, register and retrieve view/actions to user module

--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
+from tokenize import Token
+import bcrypt
 from django.http import HttpResponse
 
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.viewsets import ViewSet, ReadOnlyModelViewSet
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.authtoken.models import Token
 from users.models import User
 from users.serializers import UserSerializer
+from rest_framework.decorators import action
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
@@ -23,10 +26,24 @@ class UserViewSet(ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @action(methods=['GET'], detail=False, url_path='me')
+    def me(self, request, *args, **kwargs):
+        return Response(data={'test':'me'},status=status.HTTP_401_UNAUTHORIZED)
+    
+    @action(methods=['POST'], detail=False, url_path='login')
+    def login(self, request):
+            password, email = request.data
+            user = User.objects.all()
+            if not user:
+                return Response(data={'message': 'user not found'}, status=status.HTTP_404_NOT_FOUND)
+            hash_password = bcrypt.hashpw(password, bcrypt.gensalt(rounds=10))
+            if hash_password != user.password:
+                return Response(data={'message': 'wrong password'}, status=status.HTTP_401_UNAUTHORIZED)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
 
-class MeViewSet(ViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+class RegisterViewSet(ViewSet):
+    allowed_methods=['POST']
+    serializer_class= UserSerializer
+    queryset=User.objects.all()
 
-    def list(self, request):
-        return Response({'test': 'test'})
