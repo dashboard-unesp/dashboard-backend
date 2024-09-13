c5f0f934a3b8a28de11347660235554bbdadbce3
Rafael <rafael_dvid@hotmail.com>
2024-09-12 21:07:50 -0300

    feat: generating hash password in user creating

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,23 @@
+import bcrypt
 from rest_framework import serializers
 from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = User
         fields = ('__all__')
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        hashed_password = bcrypt.hashpw(validated_data['password'].encode('utf-8'), bcrypt.gensalt(rounds=10))
+        user.set_password(hashed_password)
+        user.save()
+        return user
+    
+
+class LoginSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
