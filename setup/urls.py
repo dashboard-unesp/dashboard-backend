3f49536365827ab1afd8bcb5267d0f46ce0b3111
Rafael <rafael_dvid@hotmail.com>
2024-11-15 00:01:05 -0300

    fix: remove old climateviewset

--- setup/urls.py
+++ setup/urls.py
@@ -6,7 +6,7 @@ from rest_framework_swagger.views import get_swagger_view
 from users.views import UserViewSet
 
 from stations.sync_data import sync_files
-from stations.views import ClimateDataViewSet, ClimateDataViewSet2
+from stations.views import ClimateDataViewSet
 
 
 schema_view = get_swagger_view(title='Unesp Dashboard API')
@@ -15,7 +15,6 @@ stations_router = DefaultRouter()
 users_router = DefaultRouter()
 
 stations_router.register('climate', ClimateDataViewSet, basename="CDT")
-stations_router.register('climate2', ClimateDataViewSet2, basename="CDT2")
 
 users_router.register('users', UserViewSet, 'UVT')
 
